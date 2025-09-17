13c7f5786c46f7f8a64b4a05748c407590737c86
Keops/KPX <83623787+KeopsA02@users.noreply.github.com>
2025-09-17 12:53:52 -0600

    Delete Python Fundamentals and Basic Mathematics (Month 1)/Introduction to Python (week 1) directory

--- Python Fundamentals and Basic Mathematics (Month 1)/Introduction to Python (week 1)/Logical_Operators.py
+++
@@ -1,7 +0,0 @@
-""" Operators used to combine conditional statements and perform logical operations. """
-
-x = True
-y = False
-print(f"AND: {x} and {y} -> {x and y}")  # False
-print(f"OR: {x} or {y} -> {x or y}")     # True
-print(f"NOT: not {x} -> {not x}")        # False
--- Python Fundamentals and Basic Mathematics (Month 1)/Introduction to Python (week 1)/While_Loop.py
+++
@@ -1,6 +0,0 @@
-""" A loop that continues executing as long as a specified condition remains True. """
-
-counter = 0
-while counter < 3:
-    print(f"Counter: {counter}")
-    counter += 1  # counter = counter + 1
